7824b63baf9f0e7d1c4a466539ed9d2374ca2bad
Luis david <david12.29@hotmail.com>
2023-08-07 15:29:35 -0500

    This is the beginnig

---
+++ src/test/java/com/secondProject/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondProject.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
